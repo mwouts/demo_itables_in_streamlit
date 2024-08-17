15d96437642e4a78d4ac6ac17f4d9cf10dd27102
Marc Wouts <marc.wouts@gmail.com>
2024-08-17 13:01:05 +0100

    Move the controls to the side bar

--- itables_app.py
+++ itables_app.py
@@ -1,90 +1,98 @@
 """Run this app with: streamlit run itables_app.py"""
 
+import itables.options as it_opt
 import pyarrow
 import streamlit as st
 from itables.sample_dfs import get_countries, get_dict_of_test_dfs
 from itables.streamlit import interactive_table
 from st_aggrid import AgGrid
-from streamlit.components.v1 import html
 from streamlit.components.v1.components import MarshallComponentException
 
-st.set_page_config(layout="wide")
-logo_col, title_col = st.columns([0.1, 0.9])
-with logo_col:
-    st.markdown(
-        "![ITables](https://raw.githubusercontent.com/mwouts/itables/main/src/itables/logo/logo.svg)"
-    )
-with title_col:
-    html(
-        """
-         <h1>
-         <a href="https://mwouts.github.io/itables">ITables</a>
-         <a class="github-button" href="https://github.com/mwouts/itables" data-icon="octicon-star" data-show-count="true"></a>
-         in Streamlit</h1>
-         <h2>Python DataFrames as Interactive <a href="https://datatables.net">DataTables</a>
-         </h2>
-        <script src="https://buttons.github.io/buttons.js"></script>"""
-    )
-
-st.header("Code snippet")
-
+st.logo(
+    "https://raw.githubusercontent.com/mwouts/itables/main/src/itables/logo/logo.svg",
+    link="https://mwouts.github.io/itables/streamlit.html",
+)
 st.markdown(
+    "![ITables](https://raw.githubusercontent.com/mwouts/itables/main/src/itables/logo/wide.svg)"
+)
+st.sidebar.markdown(
     """
-```python
-from itables.streamlit import interactive_table
-
-interactive_table(df, ...)  # add caption, buttons, style, etc ... like you do with 'show' in a notebook
-```
-"""
+                    # ITables in Streamlit
+                    ![Stars](https://img.shields.io/github/stars/mwouts/itables)
+                    """
 )
 
-st.header("Countries")
 
-caption_col, classes_col, buttons_col, style_col, render_with_col = st.columns(
-    [0.10, 0.25, 0.25, 0.20, 0.10]
-)
-caption = caption_col.text_input("Caption", value="Countries")
-classes = classes_col.multiselect(
+caption = st.sidebar.text_input("Caption", value="Countries")
+classes = st.sidebar.multiselect(
     "Classes",
     options=["display", "nowrap", "compact", "cell-border", "stripe"],
-    default=["display", "nowrap"],
+    default=it_opt.classes.split(" "),
 )
-buttons = buttons_col.multiselect(
+buttons = st.sidebar.multiselect(
     "Buttons",
     options=["pageLength", "copyHtml5", "csvHtml5", "excelHtml5", "colvis"],
     default=["copyHtml5", "csvHtml5", "excelHtml5", "colvis"],
 )
 
-style = style_col.text_input(
-    "Style", value="table-layout:auto;width:auto;margin:auto;caption-side:bottom"
-)
+style = st.sidebar.text_input("Style", value=it_opt.style)
 
-render_with = render_with_col.selectbox(
+render_with = st.sidebar.selectbox(
     "Render with", ["st.dataframe", "streamlit-aggrid", "itables"], index=2
 )
+
+it_args = {}
+if classes != it_opt.classes.split(" "):
+    it_args["classes"] = classes
+if style != it_opt.style:
+    it_args["style"] = style
+
+if buttons:
+    it_args["buttons"] = buttons
+
+include_html = st.sidebar.checkbox("Include HTML")
+
+if caption:
+    it_args = {"caption": caption, **it_args}
 
 if render_with == "st.dataframe":
 
     def interactive_table(df, **not_used):
         return st.dataframe(df)
 
+    snippet = """st.dataframe(df)
+"""
+
 elif render_with == "streamlit-aggrid":
 
     def interactive_table(df, key=None, **not_used):
         return AgGrid(df, key=key)
 
+    snippet = """from st_aggrid import AgGrid
 
-it_args = dict(
-    classes=classes,
-    style=style,
+AgGrid(df, key=None)
+"""
+else:
+    formatted_args = ["df"] + [
+        f"{key}='{value}'" if isinstance(value, str) else f"{key}={value}"
+        for key, value in it_args.items()
+    ]
+    formatted_args = ",\n                  ".join(formatted_args)
+    snippet = f"""from itables.streamlit import interactive_table
+
+interactive_table({formatted_args})
+"""
+
+st.markdown(
+    f"""```python
+{snippet}
+```
+"""
 )
-if buttons:
-    it_args["buttons"] = buttons
 
-include_html = st.checkbox("Include HTML")
 df = get_countries(html=include_html)
 
-interactive_table(df, caption=caption, **it_args)
+interactive_table(df, **it_args)
 
 st.header("More sample dataframes")
 test_dfs = get_dict_of_test_dfs()
